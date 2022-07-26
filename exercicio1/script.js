214e1d0fae30bf63e43e5773565ae856eb552d91
Bruno Ferreira <bgsf1991@gmail.com>
2022-07-26 01:22:25 -0300

    Resolução dos Exercícios 1, 2 e 3 do A009-STRINGS

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+///////////////////////// EXERCÍCIO 1 ///////////////////////////
+
+const nome = prompt('Qual é o seu nome?');
+
+const comida1 = prompt('Qual é sua comida favorita número 1?');
+const comida2 = prompt('Qual é sua comida favorita número 2?');
+const comida3 = prompt('Qual é sua comida favorita número 3?');
+
+console.log(`Estas são as comidas favoritas de ${nome}:
+1º ${comida1}
+2º ${comida2}
+3º ${comida3}`);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
-//Crie a const para a frase aqui
+///////////////////////// EXERCÍCIO 3 ///////////////////////////
+
+// a)
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+console.log('a)');
+console.log(frase);
+console.log('');
+
+// b)
+const fraseTroca1 = frase.replace('verde', 'rosa');
+const fraseTroca2 = fraseTroca1.replace('azul', 'laranja');
+console.log('b)');
+console.log(fraseTroca2);
+console.log('');
+
+// c)
+console.log('c)');
+console.log(`Inclui verde: ${fraseTroca2.includes('verde')} 
+Inclui laranja: ${fraseTroca2.includes('laranja')}`);
+console.log('');
+
+// EXTRA
+// Tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+console.log('(EXTRA)');
+console.log(fraseTroca2.slice(0, 76) + fraseTroca2.slice(76).toUpperCase());
